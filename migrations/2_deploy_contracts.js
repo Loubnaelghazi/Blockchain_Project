1adf382350813a55d25e9a302a95490624dcad5c
Loubna El Ghazi <114692135+Loubnaelghazi@users.noreply.github.com>
2024-12-16 21:25:19 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const AuditContract = artifacts.require("AuditContract");
 const PatientContract = artifacts.require("PatientContract");
 
 module.exports = async function (deployer, network, accounts) {
-  // The address to be set as the audit address (can use the first account for simplicity)
+  
   const auditAddress = "0x5D4281e40bEf3E5944144C87095a6E7C8bBD28E6";
 
   console.log("Deploying AuditContract...");
@@ -12,10 +12,10 @@ module.exports = async function (deployer, network, accounts) {
 
   console.log("Deploying PatientContract (optional)...");
 
-  // Deploy a dummy PatientContract (optional) to test interactions
+
   await deployer.deploy(PatientContract, auditAddress);
   const patientContractInstance = await PatientContract.deployed();
   console.log(`PatientContract deployed at address: ${patientContractInstance.address}`);
 
-  // Additional logic for testing or initializing contracts can go here
+  
 };
